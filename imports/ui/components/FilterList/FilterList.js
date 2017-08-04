c152fe52333bb870e1b13b791814e060c674be7a
Sonia <soniasophiaw@gmail.com>
2017-08-04 13:59:24 -0700

    Start filter list

---
+++ imports/ui/components/FilterList/FilterList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MenuItem from 'material-ui/MenuItem';
+import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+import SelectField from 'material-ui/SelectField';
+import './styles.css';
+
+const FilterList = ({ dispatch, filterValues, handleChange }) => {
+
+}
+
+export default FilterList;
+
