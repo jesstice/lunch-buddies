aab5bdd1a82e508870736586a442fa510874feb6
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-08-04 15:46:35 -0700

    add material ui to header bar

--- imports/ui/components/HeaderBar/HeaderBar.js
+++ imports/ui/components/HeaderBar/HeaderBar.js
@@ -1,3 +1,35 @@
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import FontIcon from 'material-ui/FontIcon';
 
+const style = {
+  margin: 12,
+};
+
+const HeaderBar = () => (
+  <AppBar>
+    <RaisedButton
+      backgroundColor="#a4c639"
+      icon={<FontIcon className="material-icons" />}
+      style={style}
+    />
+    <RaisedButton
+      backgroundColor="#a4c639"
+      icon={<FontIcon className="material-icons" />}
+      style={style}
+    />
+    <RaisedButton
+      label="Profile"
+      backgroundColor="#a4c639"
+      icon={<FontIcon className="material-icons" />}
+      style={style}
+    />
+    <RaisedButton
+      label="Logout"
+      backgroundColor="#a4c639"
+      style={style}
+    />
+  </AppBar>
+);
 
 export default HeaderBar;
