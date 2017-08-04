c24915e6161a6e0dfc3948c9267242c0051c02cb
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-08-04 16:02:29 -0700

    add headerbar to layout

--- imports/ui/components/Layout/Layout.js
+++ imports/ui/components/Layout/Layout.js
@@ -1,12 +1,18 @@
-//import MiniLunchInvites from '../MiniLunchInvites/MiniLunchInvites';
 import React from 'react';
+import HeaderBar from '../HeaderBar/';
+//import MiniLunchInvites from '../MiniLunchInvites/MiniLunchInvites';
 
 const Layout = ({children}) => {
-    return (
-        <div className="insideLayout">
-            {children}
-        </div>
-    ) 
+  return (
+    <div className="app-wrapper">
+      <div className="app-header">
+        <HeaderBar />
+      </div>
+      <div className="app-content">
+          {children}
+      </div>
+    </div>
+  )
 }
 
 export default Layout;
