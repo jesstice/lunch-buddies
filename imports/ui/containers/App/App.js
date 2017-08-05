5bb56099238739fe41ed616d2afa822db9444f5e
Sonia <soniasophiaw@gmail.com>
2017-08-04 17:42:07 -0700

    Accept changes

--- imports/ui/containers/App/App.js
+++ imports/ui/containers/App/App.js
@@ -16,17 +16,27 @@ import './App.css';
 import SignUp from '../../components/SignUp/SignUp';
 import FilterList from '../../components/FilterList/FilterList';
 import BuddyListItem from '../../components/BuddyListItem/BuddyListItem';
+<<<<<<< HEAD
 import muiTheme from '../../config/theme';
+=======
+>>>>>>> Start styling buddy list item
 import '../../../../client/main.html';
 
 
 injectTapEventPlugin();
 const LunchBuddies = () => (
     
+<<<<<<< HEAD
 <MuiThemeProvider muiTheme={muiTheme}>
   {/* <Layout>
   </Layout> */}
   <BuddyListItem />
+=======
+<MuiThemeProvider>
+  <Layout>
+  </Layout>
+  <BuddListItem />
+>>>>>>> Start styling buddy list item
         {/* <SignUp /> */}
 
 </MuiThemeProvider>
