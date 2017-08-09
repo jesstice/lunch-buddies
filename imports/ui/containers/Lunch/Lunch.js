f47ec6099fcc5e7d86a25fd795715f13490f7b53
Sonia <soniasophiaw@gmail.com>
2017-08-09 09:45:32 -0700

    Create lunch component

--- imports/ui/containers/Lunch/Lunch.js
+++ imports/ui/containers/Lunch/Lunch.js
@@ -1,3 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+import Gravatar from 'react-gravatar';
+import './styles.css';
+
 // TO DO: Add "Leave Lunch" button
+
+const styles = {
+  button: {
+    margin: 12,
+  }
+}
+
+const Lunch = () => {
+  return (
+    <div className="lunchWrapper">
+      <Paper zDepth={3}>
+        <div className="lunchBuddies">
+          <h1>Lunch Buddies</h1>
+          <li>
+            <Gravatar email="testemail@gmail.com" className="gravatarImage" size={150}/>
+          </li>
+        </div>
+        <h1>Lunch Details</h1>
+        <p>Budget: $$</p>
+        <p>Cuisine: Italian</p>
+        <div>
+          <RaisedButton
+            href=""
+            target="_blank"
+            label="Leave this lunch"
+            primary={true}
+            style={styles.button}
+            onTouchTap={() => removeLunch()}
+          />
+        </div>
+      </Paper>
+    </div>
+  )
+};
 
 export default Lunch;
