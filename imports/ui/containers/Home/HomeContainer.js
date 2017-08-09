c7256019b2ab6a32baa3c4bcae73ea985ea74338
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-08-09 09:45:53 -0700

    Subscribe to users publishes in fixtures

--- imports/ui/containers/Home/HomeContainer.js
+++ imports/ui/containers/Home/HomeContainer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Home from './Home';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
 
 class HomeContainer extends Component {
 
@@ -8,4 +10,9 @@ class HomeContainer extends Component {
     }
 }
 
-export default HomeContainer;
+export default createContainer(function() {
+  Meteor.subscribe('users');
+    return {
+      users: Meteor.users.find({}, { fields: {email: 1, profile: 1} }).fetch() //can use find({title: “kek”}) to specify query;
+  };
+}, HomeContainer);
