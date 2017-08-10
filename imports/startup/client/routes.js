d2c0f65f69a0507b1096173577a8825009a4b5b4
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-08-09 19:43:15 -0700

    update route with profile id route for future use

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -12,22 +12,31 @@ import Login from '../../ui/containers/Login/index';
 import SignUp from '../../ui/containers/SignUp/index';
 import Home from '../../ui/containers/Home/index';
 import Profile from '../../ui/containers/Profile/index';
+// import Lunch from '../../ui/containers/Lunch/index';
 import NotFound from '../../ui/components/NotFound/index';
+// import Public from ...
+// import Authenticated from ...
 
 const Routes = () => {
     return ( 
-        <Switch>  
+        <Switch>
           <Route exact path="/login" component={Login} /> 
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/" component={Home} />
+          {/* <Route exact path="/profile/:_id" component={Profile} /> */}
           <Route exact path="/profile" component={Profile} />
+          {/* <Route exact path="/mylunch" component={Lunch} /> */}
           <Route component={NotFound} />
+          {/* TO DO: replace when sign up configured
+          <Public exact path="/login" component={Login} />
+          <Public exact path="/signup" component={SignUp} />
+          <Authenticated exact path="/" component={Home} />
+          <Authenticated exact path="/profile/:_id" component={Profile} />
+          <Authenticated component={NotFound} />
+          <Authenticated exact path="/mylunch" component={Lunch} />
+          */}
         </Switch>
     );
 };
 
-
 export default Routes;
-
- //<Route exact path="/profile/:id" component={ProfileContainer} />
- //<Route exact path="/" component={Items} />
