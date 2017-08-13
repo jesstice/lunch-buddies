d49f7384b0439b50d40013fc29ba5540fc414ce9
Lainera <fistlain@hotmail.com>
2017-08-13 02:10:19 -0700

    Add invitation modal conditional rendering, some ux logic

--- imports/ui/components/InvitationModal/InvitationModal.js
+++ imports/ui/components/InvitationModal/InvitationModal.js
@@ -6,44 +6,76 @@ import NewLunchSelectors from '../../containers/NewLunchSelectors/';
 
 import './styles.css';
 
-const InvitationModal = ({handleLunchFlip, handleLunchCreation, handleInvitation, inviteeName}) => {
-
-  return (
-    <Dialog open={true}>
-      <ul>
-        <li>
-          <p>You are about to invite <b>{inviteeName}</b></p>
-          <div>
-            <NewLunchSelectors />
-          </div>
-          <p>Invite sent: 5 mins ago</p>
-          <div>
-            <RaisedButton
-              label="Invite"
-              primary
-              className="invitationButton"
-              icon={<i className="fa fa-check" aria-hidden="true"></i>}
-              onTouchTap={()=>{handleInvitation()}}
-            />
-            <RaisedButton
-              label="Create Lunch"
-              primary
-              className="invitationButton"
-              icon={<i className="fa fa-check" aria-hidden="true"></i>}
-              onTouchTap={()=>{handleLunchCreation()}}
-            />
-            <RaisedButton
-              label="Cancel"
-              secondary
-              className="invitationButton"
-              icon={<i className="fa fa-times" aria-hidden="true"></i>}
-              onTouchTap={()=>{handleLunchFlip()}}
-            />
-          </div>
-        </li>
-      </ul>
-    </Dialog>
-  );
+const InvitationModal = ({ handleLunchFlip, handleLunchCreation, handleInvitation, inviteeName }) => {
+  const user = Meteor.user();
+  if (user.profile.currentLunch) {
+    return (
+      <Dialog open={true}>
+        <ul>
+          <li>
+            <p>You are about to invite <b>{inviteeName}</b></p>
+            <div>
+              <p>Shall we proceed?</p>
+            </div>
+            <div>
+              <RaisedButton
+                label="Invite"
+                primary
+                className="invitationButton"
+                icon={<i className="fa fa-check" aria-hidden="true"></i>}
+                onTouchTap={() => { handleInvitation() }}
+              />
+              <RaisedButton
+                label="Cancel"
+                secondary
+                className="invitationButton"
+                icon={<i className="fa fa-times" aria-hidden="true"></i>}
+                onTouchTap={() => { handleLunchFlip() }}
+              />
+            </div>
+          </li>
+        </ul>
+      </Dialog>
+    );
+  } else {
+    return (
+      <Dialog open={true}>
+        <ul>
+          <li>
+            <p>You are about to invite <b>{inviteeName}</b></p>
+            <div>
+              <p>Select budget and cuisine preferences</p>
+              <NewLunchSelectors />
+            </div>
+            <div>
+              <RaisedButton
+                label="Invite"
+                primary
+                className="invitationButton"
+                icon={<i className="fa fa-check" aria-hidden="true"></i>}
+                onTouchTap={() => { handleInvitation() }}
+              />
+              <RaisedButton
+                label="Cancel"
+                secondary
+                className="invitationButton"
+                icon={<i className="fa fa-times" aria-hidden="true"></i>}
+                onTouchTap={() => { handleLunchFlip() }}
+              />
+            </div>
+          </li>
+        </ul>
+      </Dialog>
+    );
+  }
 }
 
 export default InvitationModal;
+
+            // <RaisedButton
+            //   label="Create Lunch"
+            //   primary
+            //   className="invitationButton"
+            //   icon={<i className="fa fa-check" aria-hidden="true"></i>}
+            //   onTouchTap={()=>{handleLunchCreation()}}
+            // />
--- imports/ui/containers/InvitationModal/InvitationModalContainer.js
+++ imports/ui/containers/InvitationModal/InvitationModalContainer.js
@@ -8,50 +8,57 @@ class InvitationalModalContainer extends Component {
   handleInvitation = () => { //check mycurrentlunch, if null > handleLunchCreation, otherwise push current lunch id into pending lunches of invitee;
     const user = Meteor.user();
     const user_id = Meteor.userId();
-    const invitee = this.props.invitee_id; //gonna be this.props.invitee;
-    user.profile.currentLunch ? Meteor.call('users.sendInvite', {user, invitee}, (error) =>{
-      if(error) {
-        console.log(error.reason);
-      } else {
-        this.props.dispatch(flipCreateLunchModal());
-      }
-    } ) : console.log('fail') //handle creation;
-   // console.log(user);
+    const invitee = this.props.invitee_id;
+    //if user has currentLunch => invite invitee to that lunch, if user does not have current lunch, create new lunch;
+    if (user.profile.currentLunch) {
+      Meteor.call('users.sendInvite', { user, invitee }, (error) => {
+        if (error) {
+          console.log(error.reason);
+        } else {
+          this.props.dispatch(flipCreateLunchModal());
+        }
+      })
+    } else {
+      this.handleLunchCreation();
+    }
   }
   handleLunchCreation = () => {
-    const user_id = Meteor.userId(); 
-    const options = { //schema checking server side
-      budget: ['under 10'], //budget form
+    const user_id = Meteor.userId();
+    const options = { // composing as object for schema checking server side
+      budget: this.props.budget_selection, //budget form
       buddies: [user_id], //storing my id
-      cuisines: ['Italian', 'Burgerland'] //cuisines form
+      cuisines: this.props.cuisine_selection //cuisines form
     }
     //also need invitee;
-    if (options && user_id) { 
+    if (options.budget.length > 0 && user_id && options.cuisines.length > 0) {
       Meteor.call('lunches.createLunch', { user_id, options }, (error) => {
         if (error) {
           console.log("There was an error: " + error.reason);
         } else {
-          console.log('possible success');//this.props.history.push('/')
+          this.handleInvitation();// this.props.history.push('/')
         }
       }
       )
+    } else {
+      this.props.dispatch(flipCreateLunchModal());
+      alert('you have to select some preferences for your lunch!')
     }
   }
   handleLunchFlip = () => {
     this.props.dispatch(flipCreateLunchModal());
   }
   render() {
-    if(this.props.showLunch) {
-    return (
-      
-      <InvitationalModal  handleLunchCreation={this.handleLunchCreation} 
-                          handleLunchFlip={this.handleLunchFlip}  
-                          handleInvitation={this.handleInvitation}
-                          inviteeName={this.props.invitee_fullname}
-                          
-      />
-      
-    )
+    if (this.props.showLunch) {
+      return (
+
+        <InvitationalModal handleLunchCreation={this.handleLunchCreation}
+          handleLunchFlip={this.handleLunchFlip}
+          handleInvitation={this.handleInvitation}
+          inviteeName={this.props.invitee_fullname}
+
+        />
+
+      )
     } else {
       return null
     }
@@ -59,10 +66,12 @@ class InvitationalModalContainer extends Component {
 }
 
 function mapStateToProps(state) {
-    return {
-      showLunch: state.lunch.showLunchInvitation,
-      invitee_id: state.lunch.invitee_id,
-      invitee_fullname: state.lunch.invitee_fullName
-    }
+  return {
+    showLunch: state.lunch.showLunchInvitation,
+    invitee_id: state.lunch.invitee_id,
+    invitee_fullname: state.lunch.invitee_fullName,
+    budget_selection: state.lunch.budget_selection,
+    cuisine_selection: state.lunch.cuisine_selection
+  }
 }
 export default connect(mapStateToProps)(InvitationalModalContainer);
