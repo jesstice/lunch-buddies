97dab67fdef519912118863dbdd444e5c5d4865e
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-08-13 14:28:00 -0700

    add lunches to publications

--- imports/startup/server/publications.js
+++ imports/startup/server/publications.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { Lunches } from '../../api/lunches/index';
 
 Meteor.publish('users', function(){
   return Meteor.users.find({}, { fields: {"emails.address": 1, profile: 1} });
