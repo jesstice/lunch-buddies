2bbe0e7cc48e5fc0b7a2bda676ce13bc3d15c1e6
Sonia <soniasophiaw@gmail.com>
2017-08-14 17:44:11 -0700

    Style home page

--- imports/ui/containers/Lunch/LunchContainer.js
+++ imports/ui/containers/Lunch/LunchContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withRouter, Redirect } from 'react-router';
+import { Redirect } from 'react-router';
 import { createContainer } from 'meteor/react-meteor-data';
 import { connect } from 'react-redux';
 import Lunch from './Lunch';
