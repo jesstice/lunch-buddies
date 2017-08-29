5f9b2b2572ceed904897ca9b916666abe5f0b1a3
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-08-29 16:41:22 -0700

    Update CSS

--- imports/ui/components/Login/Login.js
+++ imports/ui/components/Login/Login.js
@@ -10,50 +10,52 @@ import './styles.css';
 
 
 const Login = ({ handleLogin, handleEmail, handlePassword, emailError, passwordError, showEmailError, showPasswordError }) => (
-    <div className="page login">
-        <div className="cardContainer">
-            <Paper zDepth={3}>
-                <div className="formContainer">
-                    <form onSubmit={handleLogin} autoComplete="off">
-                        <div>
-                            <TextField
-                                hintText="E-mail"
-                                type="email"
-                                label="Email"
-                                name="email"
-                                errorText={emailError}
-                                onChange={handleEmail}
-                            />
-                        </div>
-                        <div>
-                            <TextField
-                                hintText="Password"
-                                type="password"
-                                label="Password"
-                                name="password"
-                                errorText={passwordError}
-                                onChange={handlePassword}
-                            />
-                        </div>
-                        <RaisedButton className="loginButton" primary fullWidth type="submit">
-                            Login
-                        </RaisedButton>
-                        <Link to={'/signup'}>
-                            <RaisedButton className="signUpButton" primary fullWidth>
-                                Sign Up
-                            </RaisedButton>
-                        </Link>
-                    </form>
-                </div>
-            </Paper>
+  <div className="page login">
+    <div className="cardContainer">
+      <Paper zDepth={3}>
+        <div className="formContainer">
+          <form onSubmit={handleLogin} autoComplete="off">
+            <div className="textField">
+              <TextField
+                className="textField"
+                hintText="E-mail"
+                type="email"
+                label="Email"
+                name="email"
+                errorText={emailError}
+                onChange={handleEmail}
+              />
+            </div>
+            <div className="textField">
+              <TextField
+                className="textField"
+                hintText="Password"
+                type="password"
+                label="Password"
+                name="password"
+                errorText={passwordError}
+                onChange={handlePassword}
+              />
+            </div>
+            <RaisedButton className="loginButton" primary fullWidth type="submit">
+              Login
+            </RaisedButton>
+            <Link to={'/signup'}>
+              <RaisedButton className="signUpButton" primary fullWidth>
+                Sign Up
+              </RaisedButton>
+            </Link>
+          </form>
         </div>
+      </Paper>
     </div>
+  </div>
 );
 
 Login.propTypes = {
-    handleLogin: PropTypes.func.isRequired,
-    handleEmail: PropTypes.func.isRequired,
-    handlePassword: PropTypes.func.isRequired
+  handleLogin: PropTypes.func.isRequired,
+  handleEmail: PropTypes.func.isRequired,
+  handlePassword: PropTypes.func.isRequired
 };
 
 export default Login;
--- imports/ui/containers/FilterList/FilterList.js
+++ imports/ui/containers/FilterList/FilterList.js
@@ -12,20 +12,36 @@ import './styles.css';
 
 class FilterList extends Component {
 
-render() {
-  if(this.props.budget&&this.props.cuisine&&this.props.interests) {
-    return (
-      <div className="filterList">
-        <Selector arr={this.props.interests} selectValues={this.props.interestsFilters} onChangeAction={filterByInterests}  hint="Filter for Interests" />
-        <Selector arr={this.props.cuisine} selectValues={this.props.cuisineFilters} onChangeAction={filterByCuisine}  hint="Filter for Cuisines" />
-        <Selector arr={this.props.budget} selectValues={this.props.budgetFilters} onChangeAction={filterByBudget}  hint="Filter for Budget" />
-      </div>
-    )
-  } else {
-    return <Loader />
+  render() {
+    if(this.props.budget && this.props.cuisine && this.props.interests) {
+      return (
+        <div className="filterList">
+          <Selector
+            arr={this.props.interests}
+            selectValues={this.props.interestsFilters}
+            onChangeAction={filterByInterests}
+            hint="Filter for Interests"
+          />
+          <Selector
+            arr={this.props.cuisine}
+            selectValues={this.props.cuisineFilters}
+            onChangeAction={filterByCuisine}
+            hint="Filter for Cuisines"
+          />
+          <Selector
+            arr={this.props.budget}
+            selectValues={this.props.budgetFilters}
+            onChangeAction={filterByBudget}
+            hint="Filter for Budget"
+          />
+        </div>
+      )
+    } else {
+      return <Loader />
+    }
   }
 }
-}
+
 const FilterWrap = createContainer(function() {
   if(Meteor.subscribe('tags').ready()) {
     const tags =  Tags.find({}, { fields: {interests: 1, cuisine: 1, budget: 1} }).fetch();
@@ -38,6 +54,7 @@ const FilterWrap = createContainer(function() {
     return {}
   }
 }, FilterList);
+
 function mapStateToProps(state) {
     return {
           interestsFilters: state.filters.interestsFilters,
